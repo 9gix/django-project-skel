0e8f1ef545195afc9f4f46f19ba526e0693cea19
Eugene <yeo.eugene.oey@gmail.com>
2012-12-02 02:49:11 +0800

    Include Default Settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -134,6 +134,13 @@ INSTALLED_APPS = (
     'south',
 )
 
+# django.contrib.auth settings
+AUTH_PROFILE_MODULE = 'accounts.UserProfile'
+LOGIN_REDIRECT_URL = '/'
+
+# Django-Registration configuration
+ACCOUNT_ACTIVATION_DAYS = 7
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
